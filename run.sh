f4aec976163fd364d1409f16e3c278ec43454105
MiniSecBGP <minisecbgp@gmail.com>
2018-09-05 17:02:17 -0300

    comment

--- run.sh
+++ run.sh
@@ -52,11 +52,14 @@ function what_to_do() {
         fi
         if [ "$var_cleaning" = "" ] || [ "$var_cleaning" = "Y" ] || [ "$var_cleaning" = "y" ] ; then
             printf '\e[1;33m%-6s\e[m\n' 'Cleaning MySQL Database...'
-	    sudo rm -R /var/lib/mysql/* 2> /dev/null
-	    sudo mysqld --initialize --explicit_defaults_for_timestamp	
+	    for i in $(sudo mysql -e "show databases" | grep -v Database | grep -v mysql | grep -v information_schema); do
+	        sudo mysql -e "drop database $i;"
+		sudo mysql -e "drop user $i@localhost;"
+	    done
+	    sudo mysql -e "flush privileges;"
 	    printf '\e[1;33m%-6s\e[m\n' 'Cleaning Nginx Configuration...'
 	    sudo find /etc/nginx/sites-available/ -type f ! -name 'default' -delete
-	    sudo find /etc/nginx/sites-enabled/ -type f ! -name 'default' -delete
+	    sudo find /etc/nginx/sites-enabled/ -type l ! -name 'default' -exec unlink {} \;
 	    sudo systemctl restart nginx
 	    printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous projects...'
 	    sudo rm -rf $BUILD_DIR/projects/*
@@ -105,7 +108,8 @@ function what_to_do() {
 	cleaning
         project_name 
 	choose_db
-        cd $var_project_path
+	mkdir $BUILD_DIR/projects 2> /dev/null
+        cd $BUILD_DIR/projects/$var_project_path
 
         printf '\n\e[1;33m%-6s\e[m\n' 'Creating Project...'
         django-admin startproject $var_project_name	
@@ -131,7 +135,10 @@ function what_to_do() {
 		$BUILD_DIR/projects/$var_project_name/$var_project_name/settings.py
             configure_MySQL	    
 	fi
-       
+
+        #sed -i -- "s/DEBUG = True/DEBUG = False/g" \
+        #     $BUILD_DIR/projects/$var_project_name/$var_project_name/settings.py
+
         printf '\n\e[1;33m%-6s\e[m\n' 'Populating Database...'
 	$BUILD_DIR/projects/$var_project_name/manage.py makemigrations
 	$BUILD_DIR/projects/$var_project_name/manage.py migrate
@@ -193,9 +200,10 @@ function what_to_do() {
     # create_project: prompts for the name of the project and the path where it will be created.
     # create_application: prompts for the name of the application, the name of the project it belongs to,
     #     and what path it will be created.
-    printf '\n\n%s\n%s\n%s\n%s\n\n%s' \
+    printf '\n\n%s\n%s\n%s\n%s\n%s\n\n%s' \
 	   'Choose what do you want to do:' '    - [1] Install Django' '    - [2] Create new Django Project' \
-	   '    - [3] Create new Application in an existing Django Project' 'Note: choose the corresponding number: '
+	   '    - [3] Create new Application in an existing Django Project' \
+	   '    - [4] Clear all projects and restore default configuration'   'Note: choose the corresponding number: '
     read var_what_to_do
 
     if [ "$var_what_to_do" = "1" ] ; then
@@ -204,6 +212,8 @@ function what_to_do() {
         create_project
     elif [ "$var_what_to_do" = "3" ] ; then
         create_application
+    elif [ "$var_what_to_do" = "4" ] ; then
+	cleaning
     else
         printf '\e[1;31m%-6s\e[m' 'error: Choose only options 1, 2 or 3 !!!'
         what_to_do	
@@ -223,6 +233,5 @@ what_to_do;
 
 # COLOCANDO EM PRODUCAO
 #settings.py
-#	ALLOWED_HOSTS = ['127.0.0.1'] #somente o proprio servidor Nginx precisa acessar o Python
 #	DEBUG = False
 
